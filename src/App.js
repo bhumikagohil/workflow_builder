d94a1346df4e57a65da8d9e1e0fbe86fecf4d7e5
Bhumika Gohil <bhumikagohil57@gmail.com>
2021-03-02 22:42:20 +0530

    final-commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router,Route,Switch} from "react-router-dom";
-import { Provider } from 'react-redux';
-import { makeStyles } from '@material-ui/core/styles';
-import {  Drawer , Grid , Typography , Paper } from '@material-ui/core';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Provider } from "react-redux";
 import store from "./redux/store";
-
+import {
+  Button,
+  Drawer,
+  makeStyles,
+  Grid,
+  Typography,
+  Paper,
+} from "@material-ui/core";
 import FlowChart from "./Components/Flowchart/index";
-import WorkflowTab from './Components/Sidebar/WorkflowTab';
-import DetailsTab from './Components/Sidebar/DetailsTab';
-import ContactsTab from './Components/Sidebar/ContactsTab';
-import SMSTab from './Components/Sidebar/SMSTab';
-import DelayTab from './Components/Sidebar/DelayTab';
-import ConditionTab from './Components/Sidebar/ConditionTab';
-
-const routes = [
-  {
-    path: "/",
-    main: () => <WorkflowTab/>
-  },
-  {
-    path: "/details",
-    main: () => <DetailsTab/>
-  },
-  {
-    path: "/contacts",
-    main: () => <ContactsTab/>
-  },
-  {
-    path: "/sms",
-    main: () => <SMSTab/>
-  },{
-    path: "/delay",
-    main: () => <DelayTab/>
-  },
-  {
-    path: "/condition",
-    main: () => <ConditionTab/>
-  },
-];
+import routes from "./Components/routes";
 
 const drawerWidth = 300;
 
@@ -49,13 +22,13 @@ const useStyles = makeStyles((theme) => ({
     width: drawerWidth,
   },
   content: {
-    display: 'flex',
+    display: "flex",
     justifyContent: "center",
     padding: theme.spacing(3),
   },
   grid: {
     height: "90%",
-    width: "100%"
+    width: "100%",
   },
   paperMain: {
     height: "98%",
@@ -71,48 +44,49 @@ function App() {
   return (
     <Provider store={store}>
       <Router>
-          <Route exact path="/">
-          <Grid container direction="row" spacing={1} >
-                <Grid item xs={2}>
-                <Drawer
-                  className={classes.drawer}
-                  variant="permanent"
-                  classes={{
-                    paper: classes.drawerPaper,
-                  }}
-                  anchor="left"
-                >
-                  <Grid container spacing={1} className={classes.grid}>
-                      <Grid item xs={12}>
-                          <Paper className={classes.paper}>
-                              <Typography>Visual Workflow Builder</Typography>
-                          </Paper>
-                      </Grid>
-                      <Grid item xs={12}>
-                          <Paper className={`${classes.paperMain} ${classes.paper}`}>
-                          <Switch>
-                          {routes.map((route, index) => (
-                            <Route
-                              key={index}
-                              path={route.path}
-                              children={<route.main />}
-                            />
-                          ))}
-                        </Switch>
-                          </Paper>
-                      </Grid>
-                  </Grid>
-                </Drawer>
+        <Grid container direction="row" spacing={1}>
+          <Grid item xs={2}>
+            <Drawer
+              className={classes.drawer}
+              variant="permanent"
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              anchor="left"
+            >
+              <Grid container spacing={1} className={classes.grid}>
+                <Grid item xs={12}>
+                  <Paper className={classes.paper}>
+                    <Typography>Visual Workflow Builder</Typography>
+                  </Paper>
                 </Grid>
-                <Grid item xs={10}>
-                  <main className={classes.content}>
-                      <FlowChart/>
-                  </main>
+                <Grid item xs={12}>
+                  <Paper className={`${classes.paperMain} ${classes.paper}`}>
+                    <Switch>
+                      {routes.map((route, index) => (
+                        // Render more <Route>s with the same paths as
+                        // above, but different components this time.
+                        <Route
+                          key={index}
+                          path={route.path}
+                          exact={route.exact}
+                          children={route.main}
+                        />
+                      ))}
+                    </Switch>
+                  </Paper>
                 </Grid>
-            </Grid>
-          </Route>
+              </Grid>
+            </Drawer>
+          </Grid>
+          <Grid item xs={10}>
+            <main className={classes.content}>
+              <FlowChart />
+            </main>
+          </Grid>
+        </Grid>
       </Router>
-   </Provider>
+    </Provider>
   );
 }
 
